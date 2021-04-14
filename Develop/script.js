505f9fb4645ef07eb466f2bf54acae57de00b94c
Jason <jrosasrjf@gmail.com>
2021-04-14 00:22:51 -0500

    commented out const fs. Unrecognized. Do not want to deal with at the moment

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 console.log("Hello workd. I am the console");
 
-const fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
+// const fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
 const inquirer = require("inquirer"); 
 
 const questionsManager = [
