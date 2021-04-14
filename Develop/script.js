a6c93e2348d681d9396ce4b57b1f6f54aa123564
Jason <jrosasrjf@gmail.com>
2021-04-14 00:13:23 -0500

    psuedo code added to the questions array

--- Develop/script.js
+++ Develop/script.js
@@ -2,3 +2,38 @@ console.log("Hello workd. I am the console");
 
 const fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
 const inquirer = require("inquirer"); 
+
+const teamQuestions = [
+    // questions needed
+
+    /*
+    first thing that happens team manager's attributes are asked
+        name
+        employee id
+        email address
+        office number
+    
+    list selection of positions 
+        engineer
+        intern
+
+    engineer card
+        name
+        ID
+        email
+        GitHub
+
+    intern card
+        name
+        ID 
+        email address
+        school 
+
+    finised building team? 
+        yes, display page 
+        no, loop again
+    */
+
+
+
+]; 
