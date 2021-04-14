a1fcdcdfa45cb6f6e339dfcacb88e70292e07ff9
Jason <jrosasrjf@gmail.com>
2021-04-14 00:35:10 -0500

    commented out require. Not sure how to install that yet

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 console.log("Hello workd. I am the console");
 
 // const fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
-const inquirer = require("inquirer"); 
+// const inquirer = require("inquirer"); 
 
 const questionsManager = [
     // questions needed
