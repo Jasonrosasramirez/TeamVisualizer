d477ab7c03557a6f1b6b10487711ac6f01bf300e
Jason <jrosasrjf@gmail.com>
2021-04-14 00:21:33 -0500

    added in objects of questions array regarding manager

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,32 @@ console.log("Hello workd. I am the console");
 const fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
 const inquirer = require("inquirer"); 
 
-const teamQuestions = [
+const questionsManager = [
     // questions needed
+
+    {
+        type: "input", 
+        message: "What is the team manager's name?",
+        name: "nameManager"
+    }, 
+
+    {
+        type: "input", 
+        message: "What is the team manager's employee ID?",
+        name: "idManager"
+    },
+    
+    {
+        type: "input", 
+        message: "What is the team manager's email address?",
+        name: "emailManager"
+    },
+
+    {
+        type: "input", 
+        message: "What is the team manager's office number?",
+        name: "phoneManager"
+    }
 
     /*
     first thing that happens team manager's attributes are asked
@@ -33,7 +57,5 @@ const teamQuestions = [
         yes, display page 
         no, loop again
     */
-
-
 
 ]; 
