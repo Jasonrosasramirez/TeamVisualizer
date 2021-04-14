7324bbe91512f3101da056f75440941310a735da
Jason <jrosasrjf@gmail.com>
2021-04-14 00:04:13 -0500

    required file system (fs)

---
+++ Develop/script.js
@@ -0,0 +1,4 @@
+console.log("Hello workd. I am the console");
+
+const fs = require("fs"); // Allows work with the file system. Can Read, create, update, delete, rename files. 
+const inquirer = require("inquirer"); 
