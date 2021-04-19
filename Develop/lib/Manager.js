e54d0359e89c86af105a222b92588e4e9fb4a777
Jason <jrosasrjf@gmail.com>
2021-04-18 21:49:34 -0500

    added comments to Manager.js for future reference

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,17 +1,18 @@
 const Employee = require("./Employee"); // imports the information from the employee script 
 
-class Manager extends Employee { 
+class Manager extends Employee { // the manager class builds upon what the employee class was. A literal externsion when the objects are created.
 
-    constructor(name, id, email, phone) { 
+    constructor(name, id, email, phone) { // Constructor builds the class using the following parameters. 
 
-        super(name, id, email); 
-        this.phone = phone;  
+        super(name, id, email); // name, id, and email are coming from the employee class script 
+        this.phone = phone; // Manager will have a unique value of needing a phone. So this can be added is like normal.  
 
     }
 
-    getrole() { return "Manager" }  
-    getPhone() { return this.phone } 
+    getrole() { return "Manager" } // returns the role of manager  
+    getPhone() { return this.phone } // returns the property of the phone number that is entered
 
 }
 
 module.exports = Manager; // allows the information to be exported and read by other scripts. 
+// more specifically, the script.js file can get information from this export. 
